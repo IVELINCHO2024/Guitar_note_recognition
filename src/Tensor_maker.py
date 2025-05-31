8d0c570c50ce2235a02485e823f99a3952daeae4
IVELINCHO2024 <157578504+IVELINCHO2024@users.noreply.github.com>
2025-05-31 11:17:22 +0300

    changing folders

--- src/Tensor_maker.py
+++ src/Tensor_maker.py
@@ -1,16 +1,19 @@
 import numpy as np
 import librosa
 import torch
+import os
 
-def mel_spectrogram(filename, note):
+def mel_spectrogram(filename):
     y, sr = librosa.load(filename, sr=22050)
 
     S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=256)
     S_db = librosa.power_to_db(S, ref=np.max)
+    return torch.tensor(S_db).unsqueeze(0)
 
-    mel_tensor = torch.tensor(S_db).unsqueeze(0)
+for root, dirs, files in os.walk(input_dir):
 
-    torch.save(mel_tensor, f'mel_tensors/{note}.pt')
 
-mel_spectrogram('data\G\guitar-single-note-g_120bpm_C_minor.wav', 'G')
+    
+
+
 
