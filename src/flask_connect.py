1dbbd9cf306ce6c0ab1afbfcec05152f54234e48
IVELINCHO2024 <157578504+IVELINCHO2024@users.noreply.github.com>
2025-06-03 12:09:26 +0300

    connecting with flask(not completed)

---
+++ src/flask_connect.py
@@ -0,0 +1,39 @@
+from flask import Flask,render_template,request,Response,redirect,url_for,session,flash
+from predict_note import predict_note
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__, template_folder='templates')
+app.secret_key = "password_integer"
+UPLOAD_FOLDER = 'uploads'
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config["SECRET_KEY"] = 'adsdsdsd'
+
+model_path = 'saved_models/note_model.pth'
+label_mapping_path = 'saved_models/label_mapping.pth'
+
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+    note = None
+    if request.method == 'POST':
+        if 'audio' not in request.files:
+             flash('Not a suitable file')
+        
+        file = request.file['audio']
+        if file.filename == '':
+            return 'File is not selected'
+        
+        if file:
+            filename = secure_filename(file.filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER', filepath])
+            file.save(filepath)
+
+        note = predict_note(model_path, label_mapping_path, filepath)
+        os.remove(filepath)
+
+    return render_template('note_ui.html', note = note)
+
+if __name__ == '__main__':
+    os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+    app.run(debug = True)
+
